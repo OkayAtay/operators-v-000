9b31330155acab51cd432f79b6d1bdbf17b90d57
Araba Taylor <araba.taylor20@gmail.com>
2017-09-30 17:23:24 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
 		if 40<speed || speed>60
-			"true"
+			true
 		end
 end
 
 
 
 def not_safe?(speed)
-	40 < speed < 60 ? "true" : "false"
+	40 < speed < 60 ? true : false
 end
